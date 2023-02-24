4dcb79c106cbc20093b6f4f78775e9faca54da98
Stewart Michaels <steward.kama@gmail.com>
2023-02-24 20:37:01 +0530

    tired. will continue tomorrow

--- 1.py
+++ 1.py
@@ -1,4 +1,6 @@
 class Item:
+    payRate = 0.8  # The payrate after giving a 20% Discount
+
     def __init__(self, name: str, price: float, quantity=0):
         # Run validations to the received arguments
         assert price >= 0, f"The price: {price} here is (-)ve. Price has to be (+)ve"
@@ -12,13 +14,12 @@ class Item:
     def calculateTotalPrice(self):
         return self.price * self.quantity
 
+    def applyDiscount(self):
+        self.price = self.price * self.payRate
 
-item1 = Item("Phone", 100, -5)
+
+item1 = Item("Phone", 100, 1)
 item2 = Item("Laptop", 1000, 3)
-
-
-print(item1.name, item1.price, item1.quantity)
-print(item1.calculateTotalPrice())
-
-print(item2.name, item2.price, item2.quantity)
-print(item2.calculateTotalPrice())
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
